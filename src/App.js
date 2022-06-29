af57161c61fbc70fe872d2b3037ac98e04a8b9af
Naman <mymail.naman@gmail.com>
2022-06-29 17:01:08 +0530

    forms

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide.js'
+import Setup from './tutorial/3-conditional-rendering/setup/4-forms.js'
 
 
 function App() {
